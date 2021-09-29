0b6bf0c45d099a29ac513cb7c160dfc6fbbb7c42
nordglede <nordglede@outlook.com>
2021-09-29 21:09:28 +0800

    feat:完成计算功能

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,31 @@
         <div class="main-item">{{ item.score }}</div>
         <div class="main-item">{{ item.credit }}</div>
       </div>
+
+      <div class="header" v-if="electiveArr.length !== 0">
+        <div class="header-item">选修</div>
+      </div>
+
+      <div class="main" v-for="(item,index) in electiveArr" :key="index">
+        <div class="main-item">{{ item.score }}</div>
+        <div class="main-item">{{ item.credit }}</div>
+      </div>
     </div>
 
     <!-- 添加新的科目 -->
     <div class="addItem">
-      <input type="text" name="score" v-model="score" />
-      <input type="text" name="credit" v-model="credit" />
-      <div @click="add" class="button">确定</div>
+      <input type="text" name="score" v-model="score" placeholder="成绩" />
+      <input type="text" name="credit" v-model="credit" placeholder="学分" />
+      <div @click="add()" class="button">确定</div>
+      <div @click="elective = !elective" class="button">切换到{{ elective ? '必修' : '选修' }}</div>
       <div @click="clear" class="button">清除全部</div>
     </div>
 
     <!-- 计算综合 -->
-    <div class=""></div>
+    <div class="sum">
+      <div class="submit" @click="submit">计算课程分</div>
+      <div class="finsh"> {{ returnVal }} </div>
+    </div>
   </div>
 </template>
 
@@ -36,17 +49,53 @@ interface suject {
 import { ref, reactive } from 'vue'
 let score = ref()
 let credit = ref()
+let returnVal = ref(0)
 
+const elective = ref(false)
 const arr: suject[] = reactive([])
+const electiveArr: suject[] = reactive([])
 const add = () => {
+  if (!score.value && !credit.value) return
   const item: suject = { score: score.value, credit: credit.value }
-  arr.push(item)
-  score.value = 0
-  credit.value = 0
+  if (elective.value) {
+    electiveArr.push(item)
+  } else {
+    arr.push(item)
+  }
+  score.value = ''
+  credit.value = ''
 }
 
 const clear = () => {
   arr.length = 0
+  electiveArr.length = 0
+  returnVal.value = 0
+}
+
+const submit = () => {
+  if (arr.length === 0) return
+
+  let bxScore = 0
+  let bxCredit = 0
+  let xxScore = 0
+  let xxCredit = 0
+  let res = 0
+  arr.forEach(item => {
+    bxScore += item.score * item.credit
+    bxCredit += item.credit
+  })
+
+  if (electiveArr.length === 0) {
+    res = bxScore / bxCredit
+  } else {
+    electiveArr.forEach(item => {
+      xxScore += item.score * item.credit
+      xxCredit += item.credit
+    })
+
+    res = ((bxScore / bxCredit) * 100 * 0.85 / 100) + ((xxScore / bxCredit) * 100 * 0.15 / 100)
+  }
+  returnVal.value = res
 }
 
 </script>
@@ -76,7 +125,7 @@ input {
 }
 
 h1 {
-  color: #ffe3e3;
+  color: #fff;
 }
 
 .table {
@@ -123,5 +172,28 @@ h1 {
   line-height: 30px;
   text-align: center;
   color: #4596b3;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.sum {
+  background: #ffe3e3;
+  border-radius: 5px;
+  margin-top: 30px;
+  width: 200px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: #4596b3;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.finsh {
+  margin-top: 60px;
+  width: 200px;
+  height: 200px;
+  font-size: 50px;
+  color: #fff;
 }
 </style>
