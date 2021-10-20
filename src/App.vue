04d06a0b63e780fc9fe266fd79010f0beb849d45
nordglede <nordglede@outlook.com>
2021-10-20 22:47:47 +0800

    feat: ver2 基本功能

--- src/App.vue
+++ src/App.vue
@@ -1,129 +1,140 @@
 <template>
   <div class="app">
-    <h1>规定课程分</h1>
-    <!-- 展示已经添加的科目 -->
-    <div class="table">
+    <h1>课程分</h1>
+    <div class="content">
       <div class="header">
-        <div class="header-item">成绩</div>
-        <div class="header-item">学分</div>
-        <div class="header-item">是否删除</div>
+        <div class="select">
+          <select v-model="courseSelected">
+            <option :value="item" v-for="item in courseList">{{ item }}</option>
+          </select>
+          <select v-model="creditSelected">
+            <option :value="item" v-for="item in creditList">{{ item }}</option>
+          </select>
+        </div>
+        <div class="button" @click="submit">添 加</div>
       </div>
-
-      <div class="main" v-for="(item,index) in arr" :key="index">
-        <div class="main-item">{{ item.score }}</div>
-        <div class="main-item">{{ item.credit }}</div>
-        <div class="main-item delede" @click="delede(item.score, item.credit, index)">删除</div>
+      <input type="text" name="input" v-model="score" placeholder="分数以空格间隔 如 90 90 89" />
+      <div class="record">
+        <!-- <div class="button" @click="calReslust">计 算</div> -->
+        <div v-if="bxArr || xxArr" class="button" @click="clear">清 空</div>
+        <!-- <div class="button" @click="calReslust">查 看</div> -->
       </div>
-
-      <div class="header" v-if="electiveArr.length !== 0">
-        <div class="header-item" style="color: #4596b3;">选修</div>
+      <div class="reslust">
+        <div class="text">{{ res }}</div>
       </div>
-
-      <div class="main" v-for="(item,index) in electiveArr" :key="index">
-        <div class="main-item">{{ item.score }}</div>
-        <div class="main-item">{{ item.credit }}</div>
-        <div class="main-item delede" @click="delede(item.score, item.credit, index)">删除</div>
-      </div>
-    </div>
-
-    <!-- 添加新的科目 -->
-    <div class="addItem">
-      <input type="number" name="score" v-model="score" placeholder="成绩" />
-      <input type="number" name="credit" v-model="credit" placeholder="学分" />
-      <div @click="add()" class="button">确定</div>
-      <div @click="elective = !elective" class="button">切换到{{ elective ? '必修' : '选修' }}</div>
-      <div @click="clear" class="button">清除全部</div>
-    </div>
-
-    <!-- 计算综合 -->
-    <div class="sum">
-      <div class="submit" @click="submit">计算课程分</div>
-      <div class="finsh">{{ returnVal }}</div>
-    </div>
-
-    <div class="footer">
-      Powered by
-      <a
-        href="https://webify.cloudbase.net/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >CloudBase Webify</a>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, Ref } from 'vue';
+
 interface suject {
-  score: number
+  score: number,
   credit: number
 }
 
-import { ref, reactive } from 'vue'
-let score = ref()
-let credit = ref()
-let returnVal = ref()
+const courseList = ['必修', '选修']
+const creditList = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]
 
-const elective = ref(false)
-const arr: suject[] = reactive([])
-const electiveArr: suject[] = reactive([])
-const add = () => {
-  if (!score.value && !credit.value) return
-  const item: suject = { score: score.value, credit: credit.value }
-  if (elective.value) {
-    electiveArr.push(item)
+const score = ref('')
+const courseSelected = ref('必修')
+const creditSelected = ref('1')
+const bxArr = ref<suject[]>([])
+const xxArr = ref<suject[]>([])
+
+const bxScore = ref<number>(0)
+const xxScore = ref<number>(0)
+const bxCredit = ref<number>(0)
+const xxCredit = ref<number>(0)
+const res = ref<number>(0)
+
+// 判断 必修/选修
+const addSuject = (courseArr: Ref<suject[]>, arr: suject[]): void => {
+  if (courseArr.value === void 0) {
+    courseArr.value = [...arr]
   } else {
-    arr.push(item)
+    courseArr.value = courseArr.value.concat(arr)
   }
-  score.value = ''
-  credit.value = ''
 }
-const delede = (score: number, credit: number, index: number) => {
-  if (arr.length !== 0 && arr[index].score === score && arr[index].credit === credit) {
-    arr.splice(index, 1)
+
+// 计算 成绩 * 学分 之和
+const calSujectSum = (arr: suject[]): number => {
+  let res = 0
+  arr.forEach(item => {
+    res += item.score * item.credit
+  })
+  return res
+}
+
+// 计算学分和
+const calCreditSum = (arr: suject[]): number => {
+  let res = 0
+  arr.forEach(item => {
+    res += item.credit
+  })
+  return res
+}
+
+// 计算课程分
+const calReslust = ():number => {
+  console.log(xxArr.value.length);
+  
+  let res = 0
+  bxScore.value = calSujectSum(bxArr.value)
+  bxCredit.value = calCreditSum(bxArr.value)
+  if (xxArr.value.length) {
+    xxScore.value = calSujectSum(xxArr.value)
+    xxCredit.value = calCreditSum(xxArr.value)
+
+    let bxTemp = (bxScore.value / bxCredit.value)
+    let xxTemp = (xxScore.value / xxCredit.value)
+    bxTemp = Math.floor(bxTemp * 100) / 100
+    xxTemp = Math.floor(xxTemp * 100) / 100
+
+    res = bxTemp * 0.85 + xxTemp * 0.15
+  } else {
+    let bxTemp = (bxScore.value / bxCredit.value)
+    bxTemp = Math.floor(bxTemp * 100) / 100
+    res = bxTemp
   }
-  if (electiveArr.length !== 0 && electiveArr[index].score === score && electiveArr[index].credit === credit) {
-    electiveArr.splice(index, 1)
+  return res
+}
+
+// 添加
+const submit = () => {
+  if (score.value === '') return
+  const scoreArr = score.value.split(' ')
+
+  const tempArr = scoreArr
+    .filter(item => !!item)
+    .map(item => {
+      return {
+        score: Number(item),
+        credit: Number(creditSelected.value)
+      }
+    })
+
+  if (courseSelected.value === '必修') {
+    addSuject(bxArr, tempArr)
+  } else {
+    addSuject(xxArr, tempArr)
   }
+
+  res.value = calReslust()
+  console.log('res',res.value);
+  
+  score.value = ''
 }
 
 const clear = () => {
-  arr.length = 0
-  electiveArr.length = 0
-  returnVal.value = ''
-}
-
-const submit = () => {
-  if (arr.length === 0) return
-
-  let bxScore = 0
-  let bxCredit = 0
-  let xxScore = 0
-  let xxCredit = 0
-  let res = 0
-  arr.forEach(item => {
-    // console.log(bxScore);
-    // console.log(bxCredit);
-
-    bxScore += item.score * item.credit
-    bxCredit += item.credit
-  })
-
-  if (electiveArr.length === 0) {
-    res = bxScore / bxCredit
-  } else {
-    electiveArr.forEach(item => {
-      xxScore += item.score * item.credit
-      xxCredit += item.credit
-    })
-
-    res = ((bxScore / bxCredit) * 100 * 0.85 / 100) + ((xxScore / xxCredit) * 100 * 0.15 / 100)
-  }
-  // console.log(bxScore);
-  // console.log(bxCredit);
-
-  // console.log(res);
-
-  returnVal.value = res.toFixed(2)
+  bxArr.value = []
+  xxArr.value = []
+  bxScore.value = 0
+  bxCredit.value = 0
+  xxScore.value = 0
+  xxCredit.value = 0
+  res.value = 0
 }
 
 </script>
@@ -135,118 +146,87 @@ html {
   padding: 0;
   width: 100%;
   height: 100%;
-  background-color: #93b5c6;
+  box-sizing: border-box;
+}
+
+input,
+select {
+  outline: none;
 }
 
 .app {
-  width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 
-input {
-  background: none;
-  outline: none;
-  border: 0px;
+.app h1 {
+  margin: 6rem 0 4rem 0;
+  font-size: 2.5rem;
 }
 
-h1 {
-  color: #fff;
-}
-
-.table {
-  background: #fff;
-  width: 50vw;
+.content {
   min-height: 30vh;
-  border-radius: 20px;
-}
-
-.table .header,
-.table .main {
+  width: 40%;
+  max-width: 70vw;
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  padding: 0.5em;
-  text-align: center;
+  flex-direction: column;
 }
 
-.addItem {
-  width: 59vw;
-  height: 100%;
-  margin-top: 60px;
+.content .header {
+  margin: 1rem 0 0.5rem;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
 }
 
-.addItem input {
-  background: #fff;
-  outline: none;
-  min-width: 50px;
-  margin-right: 10px;
-  border-radius: 5px;
-  height: 30px;
-  line-height: 30px;
+.content input {
+  height: 2rem;
+}
+
+.header .select select,
+.button {
+  width: 3.5rem;
+  height: 2rem;
+  line-height: 2rem;
   text-align: center;
   font-weight: 600;
+  font-size: 1rem;
 }
 
 .button {
-  background: #ffe3e3;
-  border-radius: 5px;
-  width: 100px;
-  height: 30px;
-  line-height: 30px;
-  text-align: center;
-  color: #4596b3;
-  font-weight: 600;
+  box-sizing: border-box;
   cursor: pointer;
+  /* border-radius: 5px; */
+  border: solid 1px #000;
 }
 
-.delede {
-  width: 6%;
-  color: #93b5c6;
+select + select,
+.button + .button {
+  margin-left: 1rem;
+}
+
+.record {
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: row;
+}
+
+.reslust {
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.reslust .text {
+  /* margin-top: 1rem; */
+  font-size: 3rem;
+  font-weight: 500;
+}
+
+.reslust .details {
   cursor: pointer;
-}
-
-.sum {
-  background: #ffe3e3;
-  border-radius: 5px;
-  margin-top: 30px;
-  width: 200px;
-  height: 50px;
-  line-height: 50px;
-  text-align: center;
-  color: #4596b3;
-  font-weight: 600;
-  cursor: pointer;
-}
-
-.finsh {
-  margin-top: 60px;
-  width: 200px;
-  height: 200px;
-  font-size: 50px;
-  color: #fff;
-}
-
-.footer {
-  bottom: 20px;
-  position: fixed;
-  color: white;
-}
-
-.footer a {
-  text-decoration: none;
-  background: linear-gradient(0.25turn, #0052d9, #00a4ff);
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-}
-
-.footer a:hover {
   text-decoration: underline;
-  color: #4596b3;
 }
 </style>
